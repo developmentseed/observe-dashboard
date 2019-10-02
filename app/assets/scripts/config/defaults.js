ac9b2bbcb251f957b443e98cd2add3874147474f
Vitor George <vitor.george@gmail.com>
2019-10-02 15:31:22 +0100

    Add default configuration file.

---
+++ app/assets/scripts/config/defaults.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export default {
+  appTitle: 'Observe Dashboard',
+  appShortTitle: 'Observe',
+  appDescription: 'A dashboard to browse and manage Observe platform data.',
+  baseUrl: 'http://localhost:9000',
+  dataServiceUrl: 'http://localhost:3000'
+};
