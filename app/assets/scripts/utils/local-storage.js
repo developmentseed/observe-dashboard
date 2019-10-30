63368800bc1101ad54d89cbc68f7100e8ab531b3
Vitor George <vitor.george@gmail.com>
2019-10-30 18:39:49 +0000

    Changes in store: persist state to local storage, remove redux-logger.

---
+++ app/assets/scripts/utils/local-storage.js
@@ -0,0 +1,20 @@
+export function loadState () {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return undefined;
+  }
+}
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (error) {
+    console.log("Couldn't save state to local storage.");
+  }
+};
