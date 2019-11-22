8412fee18b839a88f2e94b57e9a862b39824fe54
Vitor George <vitor.george@gmail.com>
2019-11-22 17:40:23 +0000

    Enable trace geojson download.

--- app/assets/scripts/components/traces/index.js
+++ app/assets/scripts/components/traces/index.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PropTypes as T } from 'prop-types';
 import styled from 'styled-components';
+import get from 'lodash.get';
 import { environment } from '../../config';
 import * as actions from '../../redux/actions/traces';
 import { showGlobalLoading, hideGlobalLoading } from '../common/global-loading';
-import { handleExportToJosm } from './utils';
+import { handleExportToJosm, downloadTrace } from './utils';
 
 import App from '../common/app';
 import { confirmDeleteItem } from '../common/confirmation-prompt';
@@ -214,6 +215,8 @@ class Traces extends React.Component {
 
   renderTableRows () {
     const { getData } = this.props.traces;
+    const { accessToken } = this.props;
+
     return getData().map(trace => {
       return (
         <tr key={trace.id}>
@@ -239,6 +242,10 @@ class Traces extends React.Component {
               useIcon='download'
               variation='primary-plain'
               size='small'
+              onClick={e => {
+                e.preventDefault();
+                downloadTrace(this.props.accessToken, trace.id);
+              }}
               hideText
             >
               Download trace
@@ -290,7 +297,8 @@ if (environment !== 'production') {
 
 function mapStateToProps (state) {
   return {
-    traces: wrapApiResult(state.traces)
+    traces: wrapApiResult(state.traces),
+    accessToken: get(state, 'authenticatedUser.data.accessToken')
   };
 }
 
