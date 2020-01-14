97bed223d3269f2b58d1e2b784c380f34f37260d
Vitor George <vitor.george@gmail.com>
2020-01-14 11:14:34 -0500

    Change API instance URL.

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -3,5 +3,5 @@
 export default {
   environment: 'staging',
   baseUrl: 'http://observe-dev.surge.sh',
-  apiUrl: 'http://observe-api-staging.us-east-1.elasticbeanstalk.com'
+  apiUrl: 'http://observe-dev.us-east-1.elasticbeanstalk.com'
 };
