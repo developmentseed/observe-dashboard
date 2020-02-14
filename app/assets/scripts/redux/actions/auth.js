cc6dffa0d648cbb40d8e0020f8d45ccb2709c2d0
Vitor George <vitor.george@gmail.com>
2020-02-14 12:11:44 +0100

    Update refreshProfile() to avoid interface flashing

--- app/assets/scripts/redux/actions/auth.js
+++ app/assets/scripts/redux/actions/auth.js
@@ -41,11 +41,50 @@ export function authenticate (accessToken) {
   });
 }
 
+/**
+ * Performs an API request to fetch updated user profile data. As this
+ * function is intended to be used in a background task to keep admin
+ * status updated, it doesn't remove user data before performing the
+ * request or when an error occur to avoid interface "flashing". The
+ * implementation is basically the same of authenticate(), but the
+ * accessToken comes from the state and request/error actions do not
+ * change state.
+ */
 export function refreshProfile () {
   return async (dispatch, getState) => {
     const state = getState();
     const accessToken = get(state, 'authenticatedUser.data.accessToken');
-    dispatch(authenticate(accessToken));
+
+    fetchDispatchFactory({
+      statePath: ['authenticatedUser'],
+      url: `${apiUrl}/profile`,
+      options: {
+        headers: {
+          Authorization: accessToken
+        }
+      },
+      receiveFn: (data, error = null) => {
+        if (error) {
+          // On error, don't change state
+          return {
+            type: 'SILENT_ERROR_AUTHENTICATED_USER'
+          };
+        } else {
+          // On success, update the profile
+          return receiveAuthenticatedProfile(data);
+        }
+      },
+      requestFn: () => {
+        // Do not change state when starting a request
+        return {
+          type: 'SILENT_REQUEST_AUTHENTICATED_USER'
+        };
+      },
+      mutator: data => ({
+        ...data,
+        accessToken // include accessToken after successful request
+      })
+    })(dispatch, getState);
   };
 }
 
